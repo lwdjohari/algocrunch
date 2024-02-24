473387cc267774ac006246662a0794befed084df
lwdjohari <linggawasistha.djohari@outlook.com>
2024-02-24 17:50:37 +0800

    Add package @algocrunch/api

---
+++ web/@algocrunch/api/src/GreeterServiceBuilder.ts
@@ -0,0 +1,63 @@
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+import { ClientOptions } from "@trident/trident-core"
+
+import { EchoServiceClientModule } from "./index";
+import { Err, Ok, Result } from "@trident/trident-core"
+
+
+export enum GreeterClientType {
+    UNKNOWN = 0,
+    ECHO_SVC_CLIENT = 1,
+}
+
+export class GreeterServiceBuilder {
+
+    static getServiceClient<T>(clientType: GreeterClientType, arg: ClientOptions): Result<T, Error>{
+        let client: T = undefined as T;
+        let transport: GrpcWebFetchTransport;
+        const is_have_endpoint = arg.endpoint() != null;
+        const is_have_credentials = arg.credentials() != null;
+        const is_has_additional_headers = arg.headers().size > 0;
+
+        if (!is_have_endpoint) {
+            return new Err (new Error('Endpoint is required!'));
+        }
+
+        if (is_have_credentials && is_has_additional_headers) {
+
+            transport = new GrpcWebFetchTransport({
+                baseUrl: arg.endpoint()!.endpoint(),
+                format: arg.format(),
+                headers: {
+                    ...arg.headerAsRecords(),
+                    [arg.credentials()!.key] :[arg.credentials()!.value]
+                }
+            });
+
+
+        } else if (is_have_credentials) {
+            transport = new GrpcWebFetchTransport({
+                baseUrl: arg.endpoint()!.endpoint(),
+                format: arg.format(),
+                headers: {
+                    [arg.credentials()!.key] :[arg.credentials()!.value]
+                }
+            });
+
+
+        } else {
+            transport = new GrpcWebFetchTransport({
+                baseUrl: arg.endpoint()!.endpoint(),
+                format: arg.format(),
+                headers: arg.headerAsRecords()
+            });
+
+        }
+
+        if (clientType === GreeterClientType.ECHO_SVC_CLIENT) {
+            client = new EchoServiceClientModule.EchoServiceClient(transport) as T;
+        }
+
+        return new Ok(client as T);
+    }
+}
---
+++ web/@algocrunch/api/tests/ServiceBuilders/greeter-service.test.ts
@@ -0,0 +1,82 @@
+import { ClientOptions } from "@algocrunch/core";
+import { GreeterServiceBuilder, GreeterClientType } from "../../src/index";
+import { EchoServiceClientModule as esc } from "../../src/index";
+
+describe('GreeterServiceBuilder', () => {
+    it('should return error if no endpoint is provided', () => {
+
+
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, new ClientOptions());
+        expect(client.isErr()).toBe(true);
+    });
+});
+
+describe('GreeterServiceBuilder', () => {
+    it('should return instance of EchoClient if endpoint is provided', () => {
+        const options = new ClientOptions()
+            .setEndpoint("http:localhost")
+            .setFormat('binary');
+
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, options);
+        expect(client.isOk()).toBe(true);
+        expect(client.unwrap()).toBeDefined();
+
+
+    });
+});
+
+describe('GreeterServiceBuilder', () => {
+    it('should return instance of EchoClient if endpoint is provided with port', () => {
+        const options = new ClientOptions()
+            .setEndpoint("http:localhost", 6060)
+            .setFormat('binary');
+
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, options);
+        expect(client.isOk()).toBe(true);
+        expect(client.unwrap()).toBeDefined();
+    });
+});
+
+describe('GreeterServiceBuilder', () => {
+    it('should return instance when credentials set', () => {
+        const options = new ClientOptions()
+            .setEndpoint("http:localhost", 6060)
+            .setFormat('binary')
+            .setCredentials('token');
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, options);
+        expect(client.isOk()).toBe(true);
+        expect(client.unwrap()).toBeDefined();
+    });
+});
+
+describe('GreeterServiceBuilder', () => {
+    it('should return instance when headers set', () => {
+        const options = new ClientOptions()
+            .setEndpoint("http:localhost", 6060)
+            .setFormat('binary')
+            .addHeaders('key', 'value');
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, options);
+        expect(client.isOk()).toBe(true);
+        expect(client.unwrap()).toBeDefined();
+    });
+});
+
+describe('GreeterServiceBuilder', () => {
+    it('should return instance when headers and credentials set', () => {
+        const options = new ClientOptions()
+            .setEndpoint("http:localhost", 6060)
+            .setFormat('binary')
+            .addHeaders('key', 'value')
+            .setCredentials('token');
+        const client = GreeterServiceBuilder
+            .getServiceClient<esc.EchoServiceClient>(GreeterClientType.ECHO_SVC_CLIENT, options);
+        expect(client.isOk()).toBe(true);
+        expect(client.unwrap()).toBeDefined();
+    });
+});
+
